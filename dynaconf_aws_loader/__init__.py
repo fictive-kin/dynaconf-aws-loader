95cf9ce39e4879c6e9d3847a720def2259e1de34
Joël Perras <joel@nerderati.com>
2023-06-06 14:32:39 -0400

    Add __version__ identifier

--- dynaconf_aws_loader/__init__.py
+++ dynaconf_aws_loader/__init__.py
@@ -2,6 +2,10 @@
 Dynaconf Loader for AWS Secrets Manager
 """
 
+from importlib.metadata import version
+
+# Pull version from the package data; canonical source is pyproject.toml
+__version__ = version(__package__)
 
 # The loader identifier, which is used to identify the provenance of compiled
 # configuration key/value pairs in the resulting ``Settings`` object.
